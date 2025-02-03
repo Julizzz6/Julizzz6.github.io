17cd70a7a1cddcf36f4dc6628d0660606932a953
Julian <juligam463@gmail.com>
2025-02-02 21:00:26 -0600

    Update

--- enviar.php
+++ enviar.php
@@ -1,26 +1,25 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recibir los datos del formulario
-    $nombre = htmlspecialchars(trim($_POST['nombre']));
-    $email = htmlspecialchars(trim($_POST['email']));
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
 
-    // Aquí puedes hacer lo que necesites con los datos, como guardarlos en una base de datos o enviar un correo electrónico.
-    
-    // Ejemplo: enviar un correo electrónico
-    $to = "juligam463@gmail.com"; // Cambia esto por tu dirección de correo
-    $subject = "Nuevo mensaje de San Valentín";
-    $message = "Nombre: $nombre\nEmail: $email";
-    $headers = "From: $email";
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
 
-    // Enviar el correo
-    if (mail($to, $subject, $message, $headers)) {
-        // Redirigir a la página de agradecimiento
-        header("Location: gracias.html");
-        exit();
-    } else {
-        echo "Error al enviar el mensaje.";
-    }
-} else {
-    echo "Método de solicitud no válido.";
-}
+$message = "Este mensaje fue enviado por: " . $nombre . " \r\n";
+$message .= "Su e-mail es: " . $email . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+$para = 'juligam463@gmail.com';
+$asunto = 'Mensaje de Mi Novia Hermosa';
+
+email($para, $asunto, utf8_decode($message), $header);
+
+header("Location:index.html");
+
+
+
+
+
 ?>
